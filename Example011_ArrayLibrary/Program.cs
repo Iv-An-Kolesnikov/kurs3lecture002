ebf6b6ae924e36a563d6909802092f6b97a4e811
Iv-An-Kolesnikov <i_a_kolesnikov@list.ru>
2022-10-21 09:56:36 +0300

    add Example 011 Array Library

---
+++ Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,50 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1,10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col) 
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int[] array = new int[10];
+ 
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+FillArray(array);//Заполнение массива случайными числами
+// array[0] = 4;
+// array[9] = 4;
+PrintArray(array);//Печать массива на экран
+Console.WriteLine();
+
+int pos = IndexOf(array, 444);
+if (pos == -1) Console.WriteLine("Элемент не найден!");
+else Console.WriteLine(pos);
